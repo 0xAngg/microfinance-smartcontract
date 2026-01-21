d70481176f53955e2e3e82e70e5d17f4d126c9b7
anggadanarp <anggadanarp@gmail.com>
2026-01-21 09:30:16 +0700

    chore: refactor ContractArguments to include chainId checks for network configurations

--- config/ContractArguments.ts
+++ config/ContractArguments.ts
@@ -1,9 +1,21 @@
 import CollectionConfig from "./CollectionConfig";
+import { network } from "hardhat";
 
-const ContractArguments = [
+const chainId = network.config.chainId;
+let ContractArguments: readonly [string, string, string];
+
+if (chainId === 31337 || chainId === 1337) {
+  ContractArguments = [
+    CollectionConfig.platformAddressForLocalHost,
+    CollectionConfig.domainEip712,
+    CollectionConfig.versionDomain,
+  ] as const;
+} else {
+  ContractArguments = [
     CollectionConfig.platformAddress,
     CollectionConfig.domainEip712,
-    CollectionConfig.versionDomain
-] as const;
+    CollectionConfig.versionDomain,
+  ] as const;
+}
 
 export default ContractArguments;
